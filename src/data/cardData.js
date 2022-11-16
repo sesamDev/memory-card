2f7436f30dbaf41c02d02ae6cb1a180b6059f708
Sebastian Hård Samils <sebastian.samils@gmail.com>
2022-11-16 12:53:02 +0100

    Finished adding titles and images

--- src/data/cardData.js
+++ src/data/cardData.js
@@ -1,10 +1,43 @@
+import C_Logo from "../assets/C_Logo.png";
+import Go_Logo from "../assets/Go_Logo.png";
+import Java_Logo from "../assets/Java_Logo.png";
+import Javascript_Logo from "../assets/Javascript_Logo.png";
+import Kotlin_Logo from "../assets/Kotlin_Logo.png";
+import Python_Logo from "../assets/Python_Logo.png";
+import Rust_Logo from "../assets/Rust-logo.png";
+import Swift_Logo from "../assets/Swift_Logo.png";
+
 export const cardData = [
   {
-    title: "Card Title",
-    img: "img link",
+    title: "C",
+    img: C_Logo,
   },
   {
-    title: "Card Title",
-    img: "img link",
+    title: "Go",
+    img: Go_Logo,
+  },
+  {
+    title: "Java",
+    img: Java_Logo,
+  },
+  {
+    title: "Javascript",
+    img: Javascript_Logo,
+  },
+  {
+    title: "Kotlin",
+    img: Kotlin_Logo,
+  },
+  {
+    title: "Python",
+    img: Python_Logo,
+  },
+  {
+    title: "Rust",
+    img: Rust_Logo,
+  },
+  {
+    title: "Swift",
+    img: Swift_Logo,
   },
 ];
