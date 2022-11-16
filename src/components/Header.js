dc66dc54b497dce076ea959f6504d746f5f04fcc
Sebastian Hård Samils <sebastian.samils@gmail.com>
2022-11-16 11:57:21 +0100

    Removed Score component from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,12 @@
 import "../styles/Header.css";
 
 import React from "react";
-import Score from "./Score";
 
 const Header = () => {
   return (
     <div className="App-header">
       <h1>Game Title</h1>
       <div>Logo</div>
-      <Score />
     </div>
   );
 };
