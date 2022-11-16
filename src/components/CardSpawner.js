436848b6deebee1d07b2b6868a1b7298e9354ae0
Sebastian Hård Samils <sebastian.samils@gmail.com>
2022-11-16 23:10:51 +0100

    Instantiate unique cards when game starts

--- src/components/CardSpawner.js
+++ src/components/CardSpawner.js
@@ -1,14 +1,23 @@
 import "../styles/CardSpawner.css";
 
+import React, { useEffect, useState } from "react";
+
 import Card from "./Card.js";
-import React from "react";
 import { cardData } from "../data/cardData";
 
 const CardSpawner = (props) => {
-  const spawnCard = () => {
+  const [cardsAvailable, setcardsAvailable] = useState(() => cardData);
+  const [cardsInPlay, setcardsInPlay] = useState(() => []);
+  console.log(cardsAvailable);
+  console.log(cardsInPlay);
+
+  const randInt = (n) => {
+    return Math.floor(Math.random() * (n - 0)) + 0;
+  };
+
+  const initCards = () => {
     let cardsToSpawn = 0;
     let cards = [];
-    //const card = cardData[Math.floor(Math.random() * cardData.length)];
     if (props.level === 1) {
       cardsToSpawn = 4;
     }
@@ -19,18 +28,30 @@ const CardSpawner = (props) => {
       cardsToSpawn = 8;
     }
 
+    let uniqueCards = cardsAvailable;
     for (let i = 0; i < cardsToSpawn; i++) {
-      cards.push(cardData[i]);
+      let card = uniqueCards[randInt(uniqueCards.length)];
+      uniqueCards = uniqueCards.filter((c) => c.title !== card.title);
+      cards.push(card);
     }
-    return cards;
+
+    return { cards, uniqueCards };
   };
-  return (
-    <div className="App-cardspawner">
-      {spawnCard().map((card) => {
-        return <Card key={card.title} title={card.title} img={card.img} />;
-      })}
-    </div>
-  );
+
+  // Only call when mounted or when you reach a new level
+  useEffect(() => {
+    let { cards, uniqueCards } = initCards();
+    setcardsInPlay(cards);
+    setcardsAvailable(uniqueCards);
+  }, [props.level]);
+
+  const spawnCard = () => {
+    return cardsInPlay.map((card) => {
+      return <Card key={card.title} title={card.title} img={card.img} />;
+    });
+  };
+
+  return <div className="App-cardspawner">{spawnCard()}</div>;
 };
 
 export default CardSpawner;
