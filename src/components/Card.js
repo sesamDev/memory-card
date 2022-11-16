4ba878621067f114ea431df2ccaa26d4283b6919
Sebastian Hård Samils <sebastian.samils@gmail.com>
2022-11-16 12:53:52 +0100

    Components now uses props

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,14 @@
+import "../styles/Card.css";
+
 import React from "react";
 
-const Card = () => {
-  return <div>Card</div>;
+const Card = (props) => {
+  return (
+    <div className="App-card">
+      <img src={props.img} alt="card" />
+      <h2>{props.title}</h2>
+    </div>
+  );
 };
 
 export default Card;
