37f6ea349721fbb0df8490d7d31d8bd827268e0e
Sebastian Hård Samils <sebastian.samils@gmail.com>
2022-11-16 19:21:06 +0100

    added states

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -1,14 +1,19 @@
 import "../styles/Gameboard.css";
 
+import React, { useEffect, useState } from "react";
+
 import CardSpawner from "./CardSpawner";
-import React from "react";
 import Score from "./Score";
 
 const Gameboard = () => {
+  const [level, setLevel] = useState(1);
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+
   return (
     <div className="App-gameboard">
       <Score />
-      <CardSpawner />
+      <CardSpawner level={level} />
     </div>
   );
 };
