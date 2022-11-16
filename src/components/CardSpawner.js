c48f1beb98655e4e1afbd653f3a8519766491975
Sebastian Hård Samils <sebastian.samils@gmail.com>
2022-11-16 19:20:57 +0100

    spawn number of cards based on level

--- src/components/CardSpawner.js
+++ src/components/CardSpawner.js
@@ -4,17 +4,31 @@ import Card from "./Card.js";
 import React from "react";
 import { cardData } from "../data/cardData";
 
-const CardSpawner = () => {
+const CardSpawner = (props) => {
+  const spawnCard = () => {
+    let cardsToSpawn = 0;
+    let cards = [];
+    //const card = cardData[Math.floor(Math.random() * cardData.length)];
+    if (props.level === 1) {
+      cardsToSpawn = 4;
+    }
+    if (props.level === 2) {
+      cardsToSpawn = 6;
+    }
+    if (props.level === 3) {
+      cardsToSpawn = 8;
+    }
+
+    for (let i = 0; i < cardsToSpawn; i++) {
+      cards.push(cardData[i]);
+    }
+    return cards;
+  };
   return (
     <div className="App-cardspawner">
-      <Card img={cardData[0].img} title={cardData[0].title} />
-      <Card img={cardData[1].img} title={cardData[1].title} />
-      <Card img={cardData[2].img} title={cardData[2].title} />
-      <Card img={cardData[3].img} title={cardData[3].title} />
-      <Card img={cardData[4].img} title={cardData[4].title} />
-      <Card img={cardData[5].img} title={cardData[5].title} />
-      <Card img={cardData[6].img} title={cardData[6].title} />
-      <Card img={cardData[7].img} title={cardData[7].title} />
+      {spawnCard().map((card) => {
+        return <Card key={card.title} title={card.title} img={card.img} />;
+      })}
     </div>
   );
 };
